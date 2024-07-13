f76f0bd8190440d04d7c213c700b632d869a752b
Lew1998 <lewfilippov@gmail.com>
2024-07-13 20:10:34 +0300

    Initial commit

---
+++ src/variables/Variables.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte b = 14;
+        short s = 27;
+        int i = 1111111111;
+        float f = 2.5f;
+        long l = 1111111111111111111L;
+        double d = 642421243.54434;
+    }
+}
